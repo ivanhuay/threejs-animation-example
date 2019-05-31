84b33d93c05c0231f309363ed5c20f2f36aa187f
Ivan Huayraña <ivan.huayrana@rga.com>
2019-05-31 14:16:06 -0300

    animation working

--- main-buffer-dynamic.js
+++ main-buffer-dynamic.js
@@ -23,7 +23,6 @@ controls.keys = {
 	BOTTOM: 83
 }
 controls.update();
-// var customControls = new CustomControls(camera);
 var light = new THREE.DirectionalLight( 0xffffff, 1 );
 light.position.set( 3, 3, 5);
 
@@ -33,9 +32,6 @@ scene.add( light2 );
 
 function getHight(x,z, t){
   var time = t? t: 1;
-  // console.log(':',x,z);
-  // var arg = (Math.pow(x,2)+Math.pow(z,2),0.5);
-  // return Math.pow(arg)*Math.sin(arg)/arg;
   // return Math.sin(0.05*x)*Math.cos(0.05*z)/0.05;
   // return 5*Math.sin(z/10) +5*Math.cos(-x/10);
   // return Math.sin(10*( Math.pow(x/100,2)+Math.pow(z/100,2)) )*20;
@@ -63,9 +59,8 @@ var positions = [];
 var normals = [];
 var colors = [];
 var color = new THREE.Color();
-var n = 800, n2 = n / 2;	// triangles spread in the cube
+var n = 800, n2 = n / 2;
 var d = 12, d2 = d / 2;
-// generateMap();
 function createTriangle(vector1, vector2, vector3){
       var x = Math.random() * n - n2;
       var y = Math.random() * n - n2;
@@ -125,7 +120,7 @@ function getRenderFromPosition(){
     positionAttribute = new THREE.Float32BufferAttribute( positions, 3 );
     var normalAttribute = new THREE.Int16BufferAttribute( normals, 3 );
     var colorAttribute = new THREE.Uint8BufferAttribute( colors, 3 );
-    normalAttribute.normalized = true; // this will map the buffer values to 0.0f - +1.0f in the shader
+    normalAttribute.normalized = true;
     colorAttribute.normalized = true;
     geometry.addAttribute( 'position', positionAttribute.setDynamic(true));
     geometry.addAttribute( 'normal', normalAttribute.setDynamic(true));
@@ -202,11 +197,6 @@ function updateGraph(time){
 
           updateNormal(x,z,normalIndex,time);
           normalIndex+=9;
-          // createTriangle(
-          //   new THREE.Vector3( x,  getHight(x,z+1), z+1 ),
-          //   new THREE.Vector3( x+1, getHight(x+1,z), z),
-          //   new THREE.Vector3( x, getHight(x,z), z )
-          // );
           i+=3;
           geometry.attributes.position.array[i] = x;
           geometry.attributes.position.array[i+1] = getHight(x,z+1,time);
@@ -222,11 +212,6 @@ function updateGraph(time){
           i+=3;
           updateSecondNormal(x,z,normalIndex,time);
           normalIndex+=9;
-          // createTriangle(
-          //   new THREE.Vector3( x,  getHight(x,z+1), z+1),
-          //   new THREE.Vector3( x+1, getHight(x+1,z+1), z+1),
-          //   new THREE.Vector3( x+1, getHight(x+1,z), z)
-          // );
         }
     }
 }
@@ -236,18 +221,15 @@ function animate() {
   time++;
   graphTime = Math.sin(time/100)*5 + 5;
   requestAnimationFrame(animate);
-  if(time<800){
-      updateGraph(graphTime);
-  }
-
+  updateGraph(graphTime);
   geometry.attributes.position.needsUpdate = true
   geometry.attributes.normal.needsUpdate = true
-  if(latestPosition.distanceTo(camera.position) > 100){
-    latestPosition = camera.position.clone();
-    updateGraph(graphTime);
-    geometry.attributes.position.needsUpdate = true
-    geometry.attributes.normal.needsUpdate = true
-  }
+  // if(latestPosition.distanceTo(camera.position) > 100){
+  //   latestPosition = camera.position.clone();
+  //   updateGraph(graphTime);
+  //   geometry.attributes.position.needsUpdate = true
+  //   geometry.attributes.normal.needsUpdate = true
+  // }
   controls.update();
   renderer.render(scene, camera);
 }
