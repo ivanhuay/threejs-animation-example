e976815ebd936d25c3c208730d1522c881a82fc0
ivanhuay <ivanhuay@gmail.com>
2024-10-07 13:22:52 -0300

    update working

--- main-buffer-dynamic.js
+++ main-buffer-dynamic.js
@@ -1,70 +1,58 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var cubes = [];
-var visualRadio = 100;
-scene.background = new THREE.Color( 0x505050 );
+import * as THREE from 'three';
 
-var renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderRadio = 200;
+scene.background = new THREE.Color( 0x505077 );
+
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
-var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshLambertMaterial({
+const material = new THREE.MeshLambertMaterial({
   color: 0x00ff00
 });
 material.color.convertSRGBToLinear();
 camera.position.set(4,30,5);
-var controls = new THREE.OrbitControls( camera );
-controls.keys = {
-  LEFT: 65,
-	UP: 87,
-	RIGHT: 68,
-	BOTTOM: 83
-}
-controls.update();
-var light = new THREE.DirectionalLight( 0xffffff, 1 );
-light.position.set( 3, 3, 5);
+
+const light = new THREE.DirectionalLight( 0xffcccc, 0.1 );
+light.position.set( 3, 40, 20);
 
 scene.add( light );
-var light2 = new THREE.AmbientLight( 0xabababa, 0.6 );
+var light2 = new THREE.AmbientLight( 0xabababa, 0.8 );
 scene.add( light2 );
 
+scene.fog = new THREE.Fog( 0xcccccc, 50, 180 );
+
+
+// function to get render height for graph points
 function getHight(x,z, t){
-  var time = t? t: 1;
+  // var time = t? t: 1;
   // return Math.sin(0.05*x)*Math.cos(0.05*z)/0.05;
-  // return 5*Math.sin(z/10) +5*Math.cos(-x/10);
+  return 5*Math.sin(z * 0.2 * t/10) +5*Math.cos(-x /10);
   // return Math.sin(10*( Math.pow(x/100,2)+Math.pow(z/100,2)) )*20;
-  return (0.1/(0.1*time))*Math.sin(10*( Math.pow((x)/100,2)+Math.pow((z)/100,2)) )*20;
+  // return (0.1/(0.1*time))* Math.sin(10*( Math.pow((x)/50,2)+Math.pow((z)/50,2)) )*20;
   // return (Math.pow(Math.abs(x),0.5))*Math.sin(10*( Math.pow(x/100,2)+Math.pow(z/100,2)) )*20;
 }
-function generateMap(){
-  var limit = 10;
-  for(var z=0; z<limit;z++){
-    if(!map[z]){
-      map[z] = [];
-    }
-    for(var x=0;x<limit;x++){
-      map[z][x] = getHight(x - 10,z - 10);
-    }
-  }
-}
-var geometry = new THREE.BufferGeometry();
-var pA = new THREE.Vector3();
-var pB = new THREE.Vector3();
-var pC = new THREE.Vector3();
-var cb = new THREE.Vector3();
-var ab = new THREE.Vector3();
-var positions = [];
-var normals = [];
-var colors = [];
-var color = new THREE.Color();
-var n = 800, n2 = n / 2;
-var d = 12, d2 = d / 2;
+
+const geometry = new THREE.BufferGeometry();
+let pA = new THREE.Vector3();
+let pB = new THREE.Vector3();
+let pC = new THREE.Vector3();
+let cb = new THREE.Vector3();
+let ab = new THREE.Vector3();
+let positions = [];
+let normals = [];
+let colors = [];
+let color = new THREE.Color();
+const n = 800, n2 = n / 2;
+const d = 12, d2 = d / 2;
+
 function createTriangle(vector1, vector2, vector3){
-      var x = Math.random() * n - n2;
-      var y = Math.random() * n - n2;
-      var z = Math.random() * n - n2;
+      const x = Math.random() * n - n2;
+      const y = Math.random() * n - n2;
+      const z = Math.random() * n - n2;
 			positions.push( vector1.x, vector1.y, vector1.z );
 			positions.push( vector2.x, vector2.y, vector2.z );
 			positions.push( vector3.x, vector3.y, vector3.z );
@@ -85,24 +73,19 @@ function createTriangle(vector1, vector2, vector3){
 			var vy = ( y / n ) + 0.5;
 			var vz = ( z / n ) + 0.5;
 			color.setRGB( vx, vy, vz );
+
 			colors.push( color.r * 255, color.g * 255, color.b * 255 );
 			colors.push( color.r * 255, color.g * 255, color.b * 255 );
       colors.push( color.r * 255, color.g * 255, color.b * 255 );
 }
 
-function cleanTriangles(){
-  triangles.forEach(function(triangle){
-    scene.remove(triangle);
-  });
-  triangles = [];
-}
-var positionAttribute;
+let positionAttribute;
+
 function getRenderFromPosition(){
-    var radio = visualRadio;
-    var startX = camera.position.x - radio;
-    var endX = camera.position.x + radio;
-    var startZ = camera.position.z - radio;
-    var endZ = camera.position.z + radio;
+    const startX = camera.position.x - renderRadio;
+    const endX = camera.position.x + renderRadio;
+    const startZ = camera.position.z - renderRadio;
+    const endZ = camera.position.z + renderRadio;
     for(var x=startX; x<endX;x++){
         for(var z=startZ; z<endZ; z++){
           createTriangle(
@@ -122,33 +105,41 @@ function getRenderFromPosition(){
     var colorAttribute = new THREE.Uint8BufferAttribute( colors, 3 );
     normalAttribute.normalized = true;
     colorAttribute.normalized = true;
-    geometry.addAttribute( 'position', positionAttribute.setDynamic(true));
-    geometry.addAttribute( 'normal', normalAttribute.setDynamic(true));
-    geometry.addAttribute( 'color', colorAttribute );
+    
+    positionAttribute.setUsage(THREE.DynamicDrawUsage);
+    normalAttribute.setUsage(THREE.DynamicDrawUsage);
+
+    geometry.setAttribute( 'position', positionAttribute);
+    geometry.setAttribute( 'normal', normalAttribute);
+    geometry.setAttribute( 'color', colorAttribute );
     geometry.computeBoundingSphere();
-    var material = new THREE.MeshPhongMaterial( {
-      color: 0xaaaaaa, specular: 0xffffff, shininess: 250,
-      side: THREE.DoubleSide, vertexColors: THREE.VertexColors
-    } );
-    var mesh = new THREE.Mesh( geometry, material );
+
+     const material = new THREE.MeshPhongMaterial({
+        color: 0xaaaaaa,
+        specular: 0xffffff,
+        shininess: 250,
+        side: THREE.DoubleSide,
+        vertexColors: true
+    });
+
+    const mesh = new THREE.Mesh( geometry, material );
     scene.add(mesh);
 }
 getRenderFromPosition();
 
-var time = 0;
-var backposition = new THREE.Vector3(0,0,0);
+let time = 0;
 function updateNormal(x,z,normalIndex,t){
-    var time = t || 1;
-    pA.set(x,getHight(x,z+1,time),z+1);
-    pB.set(x+1,getHight(x+1,z,time),z);
-    pC.set(x,getHight(x,z,time),z);
+    const iTime = t || 1;
+    pA.set(x,getHight(x,z+1,iTime),z+1);
+    pB.set(x+1,getHight(x+1,z,iTime),z);
+    pC.set(x,getHight(x,z,iTime),z);
     cb.subVectors( pC, pB );
     ab.subVectors( pA, pB );
     cb.cross( ab );
     cb.normalize();
-    var nx = cb.x;
-    var ny = cb.y;
-    var nz = cb.z;
+    let nx = cb.x;
+    let ny = cb.y;
+    let nz = cb.z;
     for(var i=0;i<3;i++){
       geometry.attributes.normal.array[normalIndex+(i*3)] = nx * 32767;
       geometry.attributes.normal.array[normalIndex+1+(i*3)] = ny * 32767;
@@ -156,10 +147,10 @@ function updateNormal(x,z,normalIndex,t){
     }
 }
 function updateSecondNormal(x,z,normalIndex,t){
-    var time = t || 1;
-    pA.set(x,getHight(x,z+1,time),z+1);
-    pB.set(x+1,getHight(x+1,z+1,time),z+1);
-    pC.set(x+1,getHight(x+1,z,time),z);
+    var iTime = t || 1;
+    pA.set(x,getHight(x,z+1,iTime),z+1);
+    pB.set(x+1,getHight(x+1,z+1,iTime),z+1);
+    pC.set(x+1,getHight(x+1,z,iTime),z);
     cb.subVectors( pC, pB );
     ab.subVectors( pA, pB );
     cb.cross( ab );
@@ -174,7 +165,7 @@ function updateSecondNormal(x,z,normalIndex,t){
     }
 }
 function updateGraph(time){
-    var radio = visualRadio;
+    var radio = renderRadio;
     var startX = camera.position.x - radio;
     var endX = camera.position.x + radio;
     var startZ = camera.position.z - radio;
@@ -215,22 +206,19 @@ function updateGraph(time){
         }
     }
 }
-var latestPosition = camera.position.clone();
-var graphTime = time;
+
+let graphTime = time;
 function animate() {
   time++;
   graphTime = Math.sin(time/100)*5 + 5;
-  requestAnimationFrame(animate);
+  // Move camera in a loop
+  camera.position.set(camera.position.x + Math.sin(graphTime / 100) * 10, camera.position.y, camera.position.z)
+  
   updateGraph(graphTime);
   geometry.attributes.position.needsUpdate = true
   geometry.attributes.normal.needsUpdate = true
-  // if(latestPosition.distanceTo(camera.position) > 100){
-  //   latestPosition = camera.position.clone();
-  //   updateGraph(graphTime);
-  //   geometry.attributes.position.needsUpdate = true
-  //   geometry.attributes.normal.needsUpdate = true
-  // }
-  controls.update();
+  
   renderer.render(scene, camera);
+  requestAnimationFrame(animate);
 }
 animate();
